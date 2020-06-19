766a97a49ccccdb22692c9cfd18c6ad4aa0ac2ab
CastrogiovanniRami <38988159+Castrogiovanni20@users.noreply.github.com>
2020-06-19 10:32:16 -0300

    Updates

---
+++ precache-manifest.c17a8d8b546988877bb5bda3f4b1e70e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e75b6d482322a7c0ef219354bb575bda",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5f97c4032222bd85a00b",
+    "url": "/portfolio/static/css/2.adf1b832.chunk.css"
+  },
+  {
+    "revision": "8ed0a5c1341d7d925971",
+    "url": "/portfolio/static/css/main.6c07b513.chunk.css"
+  },
+  {
+    "revision": "5f97c4032222bd85a00b",
+    "url": "/portfolio/static/js/2.da3fb2ab.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.da3fb2ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ed0a5c1341d7d925971",
+    "url": "/portfolio/static/js/main.728fe65e.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "69f2db4830ef65d0161c7c44d0ddb1b4",
+    "url": "/portfolio/static/media/CV_CastrogiovanniRamiro.69f2db48.pdf"
+  },
+  {
+    "revision": "8349d90cf6d9c7a42f06fe79831eb0ae",
+    "url": "/portfolio/static/media/perfil.8349d90c.png"
+  }
+]);
